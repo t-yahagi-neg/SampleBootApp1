88495109d35f03982b7befe14b2f3d914e8e6313
USER <USER@DESKTOP-JG0GFCN>
2025-07-01 17:49:01 +0900

    リスト2-6

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,5 +1,6 @@
 package com.example.sample1app;
 
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleBootApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleBootApp1Application.class, args);
-		System.out.println("Welcom to Spring!!"); // P78 リスト2-5
+		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
 	}
 
 }
