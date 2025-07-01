8f2f43eb31c8c73480b9dbf413968d12723830b9
USER <USER@DESKTOP-JG0GFCN>
2025-07-01 17:37:09 +0900

    リスト2-5

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -8,6 +8,7 @@ public class SampleBootApp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
+		System.out.println("Welcom to Spring!!"); // P78 リスト2-5
 	}
 
 }
