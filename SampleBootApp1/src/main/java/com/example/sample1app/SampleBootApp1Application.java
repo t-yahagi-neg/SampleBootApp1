c928946f3ae5545140f49bea515a87a3fa609964
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 10:44:47 +0900

    リスト2-12

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -6,16 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+
 @SpringBootApplication
 @RestController
 public class SampleBootApp1Application {
 
-	String[][] data = {
-			{"noname","no email address","0"},
-			{"taro","taro@yamada","39"},
-			{"hanako","hanako@flower","28"},
-			{"sachiko","sachiko@happy","17"},
-			{"jiro","jiro@change","6"}
+	DataObject[] data = {
+			new DataObject("noname","no email address",0),
+			new DataObject("taro","taro@yamada",39),
+			new DataObject("hanako","hanako@flower",28),
+			new DataObject("sachiko","sachiko@happy",17),
+			new DataObject("jiro","jiro@change",6)
 	};
 
 	public static void main(String[] args) {
@@ -23,10 +25,22 @@ public class SampleBootApp1Application {
 	}
 
 	@GetMapping("/{num}")
-	public String index(@PathVariable("num") int num) {
+	public DataObject index(@PathVariable("num") int num) {
 		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
-		String[] item = data[n];
-		String msg = "ID=%s. {name: %s, email: %s, age: %s}";
-		return String.format(msg, num, item[0], item[1], item[2]);
+		return data[n];
+	}
+}
+
+@Data
+class DataObject {
+	private String name;
+	private String mail;
+	private int age;
+
+	public DataObject(String name, String mail, int age) {
+		super();
+		this.name = name;
+		this.mail = mail;
+		this.age = age;
 	}
 }
