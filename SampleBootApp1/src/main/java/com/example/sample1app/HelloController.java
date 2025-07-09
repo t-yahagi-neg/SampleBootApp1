0d07e4599e748ded945b05096a2776c9bbbcb880
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 12:24:42 +0900

    リスト3-11

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -11,35 +11,18 @@ public class HelloController {
 
 	@GetMapping()
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg","フォームを送信してください。");
+		mav.addObject("msg","HelloController/indexのページです。");
 		mav.setViewName("index");
 		return mav;
 	}
 
-	@PostMapping()
-	public ModelAndView form(
-			@RequestParam(value="check1",required=false)boolean check1,
-			@RequestParam(value="radio1",required=false)String radio1,
-			@RequestParam(value="select1",required=false)String select1,
-			@RequestParam(value="select2",required=false)String[] select2,
-			ModelAndView mav) {
-		String res = "";
-		try {
-			res = "check:" + check1 +
-			" radio:" + radio1 +
-			" select:" + select1 +
-			"\nselect2:";
-		} catch (NullPointerException e) {}
-		try {
-			res += select2[0];
-			for (int i = 1; i < select2.length; i++) {
-				res += ", " + select2[i];
-			}
-		} catch (NullPointerException e) {
-			res += "null";
-		}
-		mav.addObject("msg",res);
-		mav.setViewName("index");;;
-		return mav;
+	@GetMapping("/other")
+	public String other() {
+		return "redirect:/";
+	}
+
+	@GetMapping("/home")
+	public String home() {
+		return "forward:/";
 	}
 }
