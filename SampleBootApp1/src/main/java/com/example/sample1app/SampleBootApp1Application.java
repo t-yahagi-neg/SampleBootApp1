f718981b5b7be4a331de47ff67ff898da3b35ee9
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 10:03:52 +0900

    リスト2-8

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,12 +1,15 @@
 package com.example.sample1app;
 
+import java.util.Arrays;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner.Mode;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleBootApp1Application implements CommandLineRunner {
+public class SampleBootApp1Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
@@ -15,11 +18,13 @@ public class SampleBootApp1Application implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String[] args) {
+	public void run(ApplicationArguments args) {
 		System.out.println("+----------------------------------------------------------------------+");
 		System.out.println("| this is CommandLine Runner programl. |");
 		System.out.println("+----------------------------------------------------------------------+");
-		System.out.println("[" + String.join(", ", args) + "]");
+		System.out.println(args.getOptionNames());
+		System.out.println(args.getNonOptionArgs());
+		System.out.println(Arrays.asList(args.getSourceArgs()));
 	}
 
 }
