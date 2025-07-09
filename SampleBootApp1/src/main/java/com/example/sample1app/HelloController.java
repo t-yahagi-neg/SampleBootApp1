950b8a35c589cf721d2cf0216a1353be56d452b2
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 11:17:19 +0900

    リスト2-21

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -2,12 +2,18 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
+	@GetMapping("/{temp}")
+	public String index(@PathVariable("temp") String temp) {
+		switch(temp) {
+		case "index":
+			return "index";
+		default:
+			return "other";
+		}
 	}
 }
