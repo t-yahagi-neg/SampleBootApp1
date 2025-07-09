246c23c950c1a9770010298de678aa7b470b8bfc
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 10:31:07 +0900

    リスト2-11

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -3,18 +3,30 @@ package com.example.sample1app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SampleBootApp1Application {
 
+	String[][] data = {
+			{"noname","no email address","0"},
+			{"taro","taro@yamada","39"},
+			{"hanako","hanako@flower","28"},
+			{"sachiko","sachiko@happy","17"},
+			{"jiro","jiro@change","6"}
+	};
+
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
 	}
 
-	@GetMapping("/")	// ☆追加したメソッド
-	public String index() {
-		return "Hello, Spring Boot 3!!!";
+	@GetMapping("/{num}")
+	public String index(@PathVariable("num") int num) {
+		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
+		String[] item = data[n];
+		String msg = "ID=%s. {name: %s, email: %s, age: %s}";
+		return String.format(msg, num, item[0], item[1], item[2]);
 	}
 }
