9b21cf7d12ec750190cfdc99eea3400520c03df6
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 11:03:26 +0900

    リスト2-13

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -2,45 +2,36 @@ package com.example.sample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Data;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @RestController
 public class SampleBootApp1Application {
 
-	DataObject[] data = {
-			new DataObject("noname","no email address",0),
-			new DataObject("taro","taro@yamada",39),
-			new DataObject("hanako","hanako@flower",28),
-			new DataObject("sachiko","sachiko@happy",17),
-			new DataObject("jiro","jiro@change",6)
-	};
-
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
 	}
 
-	@GetMapping("/{num}")
-	public DataObject index(@PathVariable("num") int num) {
-		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
-		return data[n];
-	}
-}
-
-@Data
-class DataObject {
-	private String name;
-	private String mail;
-	private int age;
-
-	public DataObject(String name, String mail, int age) {
-		super();
-		this.name = name;
-		this.mail = mail;
-		this.age = age;
+	@GetMapping("/")
+	public String index(
+			HttpServletRequest request,
+			HttpServletResponse response) {
+		response.setContentType(MediaType.TEXT_HTML_VALUE);
+		String content = """
+				<html>
+					<head>
+					<title>Sample App</title>
+					</head>
+					<body>
+					<h1>Sample App</h1>
+					<p>This is sample app page!</p>
+				</html>
+				""";
+		return content;
 	}
 }
