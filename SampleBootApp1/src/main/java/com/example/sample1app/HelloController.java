a841e770f933e8fdfdddbd44ca8608caa1b18e71
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 13:48:46 +0900

    リスト3-16

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -2,17 +2,17 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@GetMapping()
-	public ModelAndView index(ModelAndView mav) {
+	@GetMapping("/{month}")
+	public ModelAndView index(@PathVariable("month") int month, ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "データを表示します。");
-		String[] data = new String[] {"One","Two","Three"};
-		mav.addObject("data", data);
+		mav.addObject("msg", month + "月は？");
+		mav.addObject("month", month);
 		return mav;
 	}
 }
