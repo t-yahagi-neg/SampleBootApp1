e0c6c960ea5e31ec45e8d26ac6073b45538a9e5a
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 16:19:01 +0900

    リスト4-1

---
+++ SampleBootApp1/src/main/java/com/example/sample1app/Person.java
@@ -0,0 +1,33 @@
+package com.example.sample1app;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="people")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private long id;
+
+	@Column(length = 50, nullable = false)
+	private String name;
+
+	@Column(length = 200, nullable = true)
+	private String mail;
+
+	@Column(nullable = true)
+	private Integer age;
+
+	@Column(nullable = true)
+	private String memo;
+
+}
