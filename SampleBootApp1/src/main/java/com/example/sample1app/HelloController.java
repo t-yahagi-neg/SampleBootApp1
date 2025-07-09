976a40c5ae79aeb5e526c7a49b94ef180d6998d8
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 11:07:29 +0900

    リスト2-18

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -1,5 +1,13 @@
 package com.example.sample1app;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
 
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
