bf92420041840e6b7350e6ea80a33d07d0071520
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 11:47:01 +0900

    りすｔ　3-7

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -1,22 +1,25 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/{num}")
-	public ModelAndView index(@PathVariable("num") int num, ModelAndView mav) {
-		int total = 0;
-		for (int i = 1; i <= num; i++) {
-			total += i;
-		}
-		mav.addObject("msg", num + "までの合計を計算します。");
-		mav.addObject("content", "total: " + total);
+	@GetMapping()
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("msg","名前を書いてください。");
+		mav.setViewName("index");
+		return mav;
+	}
+
+	@PostMapping()
+	public ModelAndView form(@RequestParam("text1") String str, ModelAndView mav) {
+		mav.addObject("msg","こんにちは、" + str + "さん!");
+		mav.addObject("value",str);
 		mav.setViewName("index");
 		return mav;
 	}
