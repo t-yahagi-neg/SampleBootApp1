eb68558d5780ead9b32a36f997086cd624fbaef3
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 17:28:20 +0900

    リスト4-9

--- SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.example.sample1app.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.sample1app.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
+	public Optional<Person> findById(Long name);
 }
