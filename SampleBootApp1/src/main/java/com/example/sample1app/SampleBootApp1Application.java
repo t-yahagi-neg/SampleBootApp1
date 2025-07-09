b5c27e620ca6356338462116166b65c06c65d56e
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 11:05:21 +0900

    リスト2-16

--- SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -2,12 +2,7 @@ package com.example.sample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @RestController
@@ -15,23 +10,5 @@ public class SampleBootApp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
-	}
-
-	@GetMapping("/")
-	public String index(
-			HttpServletRequest request,
-			HttpServletResponse response) {
-		response.setContentType(MediaType.TEXT_HTML_VALUE);
-		String content = """
-				<html>
-					<head>
-					<title>Sample App</title>
-					</head>
-					<body>
-					<h1>Sample App</h1>
-					<p>This is sample app page!</p>
-				</html>
-				""";
-		return content;
 	}
 }
