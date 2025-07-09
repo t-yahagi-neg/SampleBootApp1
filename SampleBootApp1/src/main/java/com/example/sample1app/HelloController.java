1a906c0bf3cf179fdf81ef006b22a5e6bb6e5148
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 11:25:36 +0900

    リスト3-2

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -1,19 +1,16 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/{temp}")
-	public String index(@PathVariable("temp") String temp) {
-		switch(temp) {
-		case "index":
-			return "index";
-		default:
-			return "other";
-		}
+	@GetMapping()
+	public String index(Model model) {
+		model.addAttribute("msg", "これはコントローラーに用意したメッセージです。");
+		return "index";
 	}
 }
