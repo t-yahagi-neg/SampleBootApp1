ee79ac8a779d4a81828dbcf27ff64b747b8ba36c
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 13:36:05 +0900

    リスト3-12

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -2,27 +2,18 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+	private boolean flag = false;
 
 	@GetMapping()
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg","HelloController/indexのページです。");
+		flag = !flag;
 		mav.setViewName("index");
+		mav.addObject("flag", flag);
+		mav.addObject("msg", "サンプルのメッセージです。");
 		return mav;
-	}
-
-	@GetMapping("/other")
-	public String other() {
-		return "redirect:/";
-	}
-
-	@GetMapping("/home")
-	public String home() {
-		return "forward:/";
 	}
 }
