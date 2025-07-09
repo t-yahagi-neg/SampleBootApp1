ce1c842a095c08f182153bc9ec4234219d101a54
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 17:57:51 +0900

    idをIDENTITYに修正、データベースでAUTO_INCREMENTの設定を追加

--- SampleBootApp1/src/main/java/com/example/sample1app/Person.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/Person.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Person {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private long id;
 
