d0c47080f2011967fa5f7d3434862b57987f9cf4
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-09 16:50:44 +0900

    リスト4-6

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -3,9 +3,13 @@ package com.example.sample1app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.sample1app.repositories.PersonRepository;
+
+import jakarta.transaction.Transactional;
 
 @Controller
 public class HelloController {
@@ -14,12 +18,21 @@ public class HelloController {
 	PersonRepository repository;
 
 	@GetMapping()
-	public ModelAndView index(ModelAndView mav) {
+	public ModelAndView index(@ModelAttribute("formModel") Person Person,
+			ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("title", "Hello page");
 		mav.addObject("msg", "this is JPA sample data.");
 		Iterable<Person> list = repository.findAll();
 		mav.addObject("data", list);
 		return mav;
+	}
+
+	@PostMapping()
+	@Transactional
+	public ModelAndView form(@ModelAttribute("formModel") Person Person,
+			ModelAndView mav) {
+		repository.saveAndFlush(Person);
+		return new ModelAndView("redirect:/");
 	}
 }
