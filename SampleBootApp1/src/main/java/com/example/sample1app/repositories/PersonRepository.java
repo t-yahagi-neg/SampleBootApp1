5d12afb3292dca066df4493120f39e0dc25fff22
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-10 14:33:16 +0900

    リスト5-17

--- SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.sample1app.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+	@Query("SELECT d FROM Person d ORDER BY d.name")
+	List<Person> findAllOrderByName();
+
 	public Optional<Person> findById(Long name);
 	public List<Person> findByNameLike(String name);
 	public List<Person> findByIdIsNotNullOrderByIdDesc();
