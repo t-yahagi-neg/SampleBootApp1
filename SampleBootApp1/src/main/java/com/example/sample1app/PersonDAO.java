0844bb79e5c1d50aae93b19c99c1e1222805a1c8
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-10 14:51:09 +0900

    リスト5-20

--- SampleBootApp1/src/main/java/com/example/sample1app/PersonDAO.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/PersonDAO.java
@@ -9,5 +9,6 @@ public interface PersonDAO <T> extends Serializable {
 	public T findById(long id);
 	public List<T> findByName(String name);
 	public List<T> find(String fstr);
+	public List<T> findByAge(int min, int max);
 
 }
