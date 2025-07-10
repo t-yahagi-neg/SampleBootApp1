51e913caf2aa9e5d6f6d69c4e15ff7a9f1379926
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-10 10:02:51 +0900

    リスト4-13

--- SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.example.sample1app.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,8 @@ import com.example.sample1app.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	public Optional<Person> findById(Long name);
+	public List<Person> findByNameLike(String name);
+	public List<Person> findByIdIsNotNullOrderByIdDesc();
+	public List<Person> findByAgeGreaterThan(Integer age);
+	public List<Person> findByAgeBetween(Integer age1, Integer age2);
 }
