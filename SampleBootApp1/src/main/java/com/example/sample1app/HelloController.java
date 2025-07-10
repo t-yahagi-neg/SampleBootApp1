2d2712cc6c0b0fab36fb912bd2f178d0294deb72
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-10 13:49:49 +0900

    リスト5-11

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -49,7 +49,7 @@ public class HelloController {
 			mav.addObject("title", "Find result");
 			mav.addObject("msg", "「" + param + "」の検索結果");
 			mav.addObject("value", param);
-			List<Person> list = dao.findByName(param);
+			List<Person> list = dao.find(param);
 			mav.addObject("data", list);
 		}
 		return mav;
