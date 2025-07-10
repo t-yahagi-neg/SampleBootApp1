ec479971d44a42fc57ffec41af4d8b96e48453f1
t-yahagi-neg <t.yahagi.ng@gmail.com>
2025-07-10 12:03:07 +0900

    リスト5-2

---
+++ SampleBootApp1/src/main/java/com/example/sample1app/PersonDAOPersonImpl.java
@@ -0,0 +1,31 @@
+package com.example.sample1app;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+@Repository
+public class PersonDAOPersonImpl implements PersonDAO<Person> {
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public PersonDAOPersonImpl() {
+		super();
+	}
+
+	@Override
+	public List<Person> getAll() {
+		Query query = entityManager.createQuery("from Person");
+		@SuppressWarnings("unchecked")
+		List<Person> list = query.getResultList();
+		entityManager.close();
+		return list;
+	}
+
+}
